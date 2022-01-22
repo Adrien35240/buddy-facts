bfb90bfc9b36682f46528d567d7ad1657b969570
Adrien35240 <adriendev35240@gmail.com>
2022-01-23 00:30:10 +0100

    fix:variable env for postgres connection

--- app/database.js
+++ app/database.js
@@ -1,9 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
-try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  define: {
+    underscored: true,
+  },
+});
+module.exports = sequelize;

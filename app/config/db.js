1747e9eb54df2112439320edfe254138abf0a18e
Adrien35240 <adriendev35240@gmail.com>
2022-02-09 18:57:46 +0100

    add config db

---
+++ app/config/db.js
@@ -0,0 +1,8 @@
+const { Pool } = require('pg');
+
+const config = {
+    connectionString: process.env.DATABASE_URL,
+    // ssl: { rejectUnauthorized: false },
+};
+
+module.exports = new Pool(config);

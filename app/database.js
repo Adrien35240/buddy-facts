5cca4949a9ab616ec51f6393bd1fb4c0d38e007e
Adrien35240 <adriendev35240@gmail.com>
2022-02-09 14:37:21 +0100

    fix:env variable

--- app/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  define: {
-    underscored: true,
-  },
-});
-module.exports = sequelize;
